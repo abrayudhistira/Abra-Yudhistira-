f8add2dd418ea8853108a8b70f0aafba604ebee9
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-03-07 09:13:56 +0700

    first commit

---
+++ 20220140102Abra.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Begin
+    //numeric Luas,p,l
+    double Luas, p, l;
+    //display "Masukkan Panjangnya"
+    cout << "Masukkan Panjangnya = ";
+    //accept p
+    cin >> p;
+    //display "Masukkan Lebarnya"
+    cout << "Masukkan Luasnya = ";
+    //accept l
+    cin >> l;
+    //compute Luas = p*l
+    Luas = p*l;
+    cout << "Luasnya = " << Luas;
+    //end
+}
